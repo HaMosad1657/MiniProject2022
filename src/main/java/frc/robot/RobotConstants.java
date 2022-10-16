f73e69073ca7724b11032926fb8edd0c3796de7b
Asriel-dmr <jonjonberg10@gmail.com>
2022-10-16 10:17:59 +0300

    Added controller

---
+++ src/main/java/frc/robot/RobotConstants.java
@@ -0,0 +1,6 @@
+
+package frc.robot;
+
+public final class RobotConstants {
+    public static final int kControllerUSBPort = 0;
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,27 +1,30 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
+
 /**
  * BUTTONS BINDINGS:
- * 
- * 
- * 
- * 
- * 
- * 
+ * Triangle - 
+ * Circle -
+ * Ex - 
+ * Square -
  */
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
-  Command m_autoCommand;
+  public static PS4Controller controller;
 
   public RobotContainer() {
-
+    controller = new PS4Controller(RobotConstants.kControllerUSBPort);
     configureButtonBindings();
     setDefaultCommands();
   }
 
   private void configureButtonBindings() {
+
   }
 
   private void setDefaultCommands() {
