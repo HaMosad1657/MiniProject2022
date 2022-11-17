a691aed6c27c5620ee9026775a60af603e04bab1
Asriel-dmr <jonjonberg10@gmail.com>
2022-11-17 19:50:03 +0200

    Added HamosadLib

--- src/main/java/frc/robot/subsystems/colorSensor/ColorSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/colorSensor/ColorSensorSubsystem.java
@@ -5,13 +5,16 @@
 package frc.robot.subsystems.colorSensor;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.ColorSensorV3;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
+import com.hamosad1657.lib.sensors.HaColorSensor;
+// import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorSensorV3.RawColor;
+
+// import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.util.Color;
+// import edu.wpi.first.wpilibj.util.Color;
 //import frc.robot.subsystems.colorSensor.ColorSensorConstants;
 
 //https://github.com/REVrobotics/Color-Sensor-v3-Examples/blob/master/Java/Read%20RGB%20Values/src/main/java/frc/robot/Robot.java
@@ -27,75 +30,101 @@ public class ColorSensorSubsystem extends SubsystemBase {
 		return instance;
 	}
 
+	private final HaColorSensor colorSensor;
 	private final I2C.Port i2cPort;
-	private final ColorSensorV3 m_colorSensor;
-	private Color detectedColor;
+	// private final ColorSensorV3 m_colorSensor;
+	// private Color detectedColor;
 	private final ShuffleboardTab tab;
-	private final NetworkTableEntry red, blue, green, proximity;
-	private double proximityVal;
+	// private final NetworkTableEntry red, blue, green, proximity;
+	// private double proximityVal;
 
 	private ColorSensorSubsystem() {
 		this.tab = Shuffleboard.getTab("ColorSensor");
 		i2cPort = I2C.Port.kMXP;
-		m_colorSensor = new ColorSensorV3(i2cPort);
-		this.blue = this.tab.add("Blue", 0.0).getEntry();
-		this.red = this.tab.add("Red", 0.0).getEntry();
-		this.green = this.tab.add("Green", 0.0).getEntry();
-		this.proximity = this.tab.add("Proximity", 0.0).getEntry();
+		this.colorSensor = new HaColorSensor(this.i2cPort, this.tab);
+		// m_colorSensor = new ColorSensorV3(i2cPort);
+		// this.blue = this.tab.add("Blue", 0.0).getEntry();
+		// this.red = this.tab.add("Red", 0.0).getEntry();
+		// this.green = this.tab.add("Green", 0.0).getEntry();
+		// this.proximity = this.tab.add("Proximity", 0.0).getEntry();
 	}
 
-	public Color getColor() {
-		return detectedColor;
+	public RawColor getColor() {
+		return this.colorSensor.getColor();
 	}
 
 	public double getRed() {
-		return detectedColor.red;
+		return this.colorSensor.getRed();
 	}
 
 	public double getBlue() {
-		return detectedColor.blue;
+		return this.colorSensor.getBlue();
 	}
 
 	public double getGreen() {
-		return detectedColor.green;
-	}
-
-	private void setProximity(double gottenProximity) {
-		this.proximityVal = gottenProximity;
-	}
-
-	public double getProximity() {
-		return proximityVal;
-	}
-
-	private boolean checkRedValues() {
-		return detectedColor.red <= ColorSensorConstants.kMaxRedTarget
-				&& detectedColor.red >= ColorSensorConstants.kMinRedTarget;
-	}
-
-	private boolean checkGreenValues() {
-		return detectedColor.green <= ColorSensorConstants.kMaxGreenTarget
-				&& detectedColor.green >= ColorSensorConstants.kMinGreenTarget;
-	}
-
-	private boolean checkBlueValues() {
-		return detectedColor.blue <= ColorSensorConstants.kMaxBlueTarget
-				&& detectedColor.blue >= ColorSensorConstants.kMinBlueTarget;
+		return this.colorSensor.getGreen();
 	}
 
 	public boolean checkForBlue() {
-		if (checkRedValues() && checkBlueValues() && checkGreenValues()) // change to the real checking algorithem
-			return true;
-		return false;
+		return this.colorSensor.isColorInRange(ColorSensorConstants.minBlue, ColorSensorConstants.maxBlue);
 	}
+
+	public double getProximity() {
+		return this.colorSensor.getProximity();
+	}
+	// public Color getColor() {
+	// return detectedColor;
+	// }
+
+	// public double getRed() {
+	// return detectedColor.red;
+	// }
+
+	// public double getBlue() {
+	// return detectedColor.blue;
+	// }
+
+	// public double getGreen() {
+	// return detectedColor.green;
+	// }
+
+	// private void setProximity(double gottenProximity) {
+	// this.proximityVal = gottenProximity;
+	// }
+
+	// public double getProximity() {
+	// return proximityVal;
+	// }
+
+	// private boolean checkRedValues() {
+	// return detectedColor.red <= ColorSensorConstants.kMaxRedTarget
+	// && detectedColor.red >= ColorSensorConstants.kMinRedTarget;
+	// }
+
+	// private boolean checkGreenValues() {
+	// return detectedColor.green <= ColorSensorConstants.kMaxGreenTarget
+	// && detectedColor.green >= ColorSensorConstants.kMinGreenTarget;
+	// }
+
+	// private boolean checkBlueValues() {
+	// return detectedColor.blue <= ColorSensorConstants.kMaxBlueTarget
+	// && detectedColor.blue >= ColorSensorConstants.kMinBlueTarget;
+	// }
+
+	// public boolean checkForBlue() {
+	// if (checkRedValues() && checkBlueValues() && checkGreenValues()) // change to
+	// the real checking algorithem
+	// return true;
+	// return false;
+	// }
 
 	@Override
 	public void periodic() {
-		detectedColor = m_colorSensor.getColor();
-		setProximity(m_colorSensor.getProximity());
-		this.blue.setDouble(getBlue());
-		this.red.setDouble(getRed());
-		this.green.setDouble(getGreen());
-		this.proximity.setDouble(getProximity());
+		// detectedColor = m_colorSensor.getColor();
+		// setProximity(m_colorSensor.getProximity());
+		// this.blue.setDouble(getBlue());
+		// this.red.setDouble(getRed());
+		// this.green.setDouble(getGreen());
+		// this.proximity.setDouble(getProximity());
 	}
 }
