2c034ad959bec7c58fbe3d6449025a3edc1c5804
Shaked <96763878+ShakedMev@users.noreply.github.com>
2022-12-22 21:01:29 +0200

    Added simulation for HaTalonFX

--- src/main/java/com/hamosad1657/lib/motors/HaTalonFX.java
+++ src/main/java/com/hamosad1657/lib/motors/HaTalonFX.java
@@ -4,6 +4,7 @@ package com.hamosad1657.lib.motors;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXSimCollection;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.hamosad1657.lib.HaUnitConvertor;
 import com.hamosad1657.lib.HaUnits.PIDGains;
@@ -23,6 +24,10 @@ public class HaTalonFX extends HaBaseTalon {
 	private double encoderTicksPerRev;
 	private double percentOutput;
 
+	private TalonFXSimCollection simMotor;
+	private double simVelocity = 0;
+	private double simPosition = 0;
+
 	/**
 	 * This class ony supports the integrated encoder or a CANCoder / another CTRE magnetic encoder as feedback devices.
 	 * If you want support for other feedback devices, add it yourself👍
@@ -30,6 +35,8 @@ public class HaTalonFX extends HaBaseTalon {
 	public HaTalonFX(WPI_TalonFX motor, PIDGains PIDGains, double wheelRadiusMeters, FeedbackDevice feedbackDevice) {
 		this.motor = motor;
 		this.motor.configSelectedFeedbackSensor(feedbackDevice);
+		this.simMotor = this.motor.getSimCollection();
+		
 		this.wheelRadiusM = wheelRadiusMeters;
 		this.configPID(PIDGains);
 		switch (feedbackDevice) {
@@ -190,5 +197,81 @@ public class HaTalonFX extends HaBaseTalon {
 			case kCoast:
 				this.motor.setNeutralMode(NeutralMode.Coast);
 		}
+	}
+
+	public void setSim(double value, Velocity type) {
+		switch (type) {
+			case kMPS:
+				value = (HaUnitConvertor.MPSToRPM(value, this.wheelRadiusM) * this.encoderTicksPerRev) / 600;
+				this.simMotor.setIntegratedSensorVelocity((int)value);
+				this.simVelocity = value;
+				break;
+			case kRPM:
+				value = (value * this.encoderTicksPerRev) / 600;
+				this.simVelocity = value;
+				break;
+			case kDegPS:
+				value = (HaUnitConvertor.degPSToRPM(value) * this.encoderTicksPerRev) / 600;
+				this.simMotor.setIntegratedSensorVelocity((int)value);
+				this.simVelocity = value;
+				break;
+			case kRadPS:
+				value = (HaUnitConvertor.radPSToRPM(value) * this.encoderTicksPerRev) / 600;
+				this.simMotor.setIntegratedSensorVelocity((int)value);
+				this.simVelocity = value;
+				break;
+		}
+	}
+
+	public void setSim(double value, Position type) {
+		switch (type) {
+			case kDegrees:
+				value = (value / 360) * this.encoderTicksPerRev;
+				this.simMotor.setIntegratedSensorRawPosition((int)value);
+				this.simPosition = value;
+				break;
+			case kRad:
+				value = (value / (Math.PI * 2)) * this.encoderTicksPerRev;
+				this.simMotor.setIntegratedSensorRawPosition((int)value);
+				this.simPosition = value;
+				break;
+			case kRotations:
+				value = value * this.encoderTicksPerRev;
+				this.simMotor.setIntegratedSensorRawPosition((int)value);
+				this.simPosition = value;
+				break;
+		}
+	}
+
+	public double getSim(Velocity type) {
+		switch (type) {
+			case kMPS:
+				return HaUnitConvertor.RPMToMPS(this.simVelocity * 600 / this.encoderTicksPerRev,
+						this.wheelRadiusM);
+			case kRPM:
+				return this.simVelocity * 600 / this.encoderTicksPerRev;
+			case kDegPS:
+				return HaUnitConvertor
+						.RPMToDegPS(this.simVelocity * 600 / this.encoderTicksPerRev);
+			case kRadPS:
+				return HaUnitConvertor
+						.RPMToRadPS(this.simVelocity * 600 / this.encoderTicksPerRev);
+			default:
+				return 0;
+		}
+	}
+
+	public double getSim(Position type) {
+		switch (type) {
+			case kDegrees:
+				return (this.simPosition / this.encoderTicksPerRev) * 360;
+			case kRad:
+				return (this.simPosition / this.encoderTicksPerRev) * (Math.PI * 2);
+			case kRotations:
+				return this.simPosition / this.encoderTicksPerRev;
+			default:
+				break;
+		}
+		return 0;
 	}
 }
